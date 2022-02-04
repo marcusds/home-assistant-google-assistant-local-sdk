2208c5d9a730c5c9bb1939cea1e70d957d851ae8
Marcus <git@marcusds.ca>
2022-02-04 00:31:18 -0800

    Fix formatting.

--- app.ts
+++ app.ts
@@ -10,249 +10,249 @@ import IntentFlow = smarthome.IntentFlow;
 import HttpResponseData = smarthome.DataFlow.HttpResponseData;
 
 const findHassCustomDeviceDataByMdnsData = (
-  requestId: string,
-  devices: Array<{ customData?: unknown }>,
-  mdnsScanData: { [key: string]: string }
+	requestId: string,
+	devices: Array<{ customData?: unknown }>,
+	mdnsScanData: { [key: string]: string }
 ) => {
-  let device;
-  device = devices.find((dev) => {
-    const customData = dev.customData as HassCustomDeviceData;
-    return (
-      customData &&
-      "webhookId" in customData &&
-      (!mdnsScanData.uuid || customData.uuid === mdnsScanData.uuid) &&
-      (!mdnsScanData.baseUrl || customData.baseUrl === mdnsScanData.baseUrl)
-    );
-  });
+	let device;
+	device = devices.find((dev) => {
+		const customData = dev.customData as HassCustomDeviceData;
+		return (
+			customData &&
+			"webhookId" in customData &&
+			(!mdnsScanData.uuid || customData.uuid === mdnsScanData.uuid) &&
+			(!mdnsScanData.baseUrl || customData.baseUrl === mdnsScanData.baseUrl)
+		);
+	});
 
-  // backwards compatibility for HA < 0.109
-  if (!device) {
-    device = devices.find(
-      (dev) =>
-        dev.customData &&
-        "webhookId" in (dev.customData as HassCustomDeviceData)
-    );
-  }
+	// backwards compatibility for HA < 0.109
+	if (!device) {
+		device = devices.find(
+			(dev) =>
+				dev.customData &&
+				"webhookId" in (dev.customData as HassCustomDeviceData)
+		);
+	}
 
-  if (!device) {
-    console.log(requestId, "Unable to find HASS connection info.", devices);
-    throw new IntentFlow.HandlerError(
-      requestId,
-      "invalidRequest",
-      "Unable to find HASS connection info."
-    );
-  }
+	if (!device) {
+		console.log(requestId, "Unable to find HASS connection info.", devices);
+		throw new IntentFlow.HandlerError(
+			requestId,
+			"invalidRequest",
+			"Unable to find HASS connection info."
+		);
+	}
 
-  return device.customData as HassCustomDeviceData;
+	return device.customData as HassCustomDeviceData;
 };
 
 const findHassCustomDeviceDataByDeviceId = (
-  requestId: string,
-  devices: Array<{ customData?: unknown }>,
-  deviceId?: string
+	requestId: string,
+	devices: Array<{ customData?: unknown }>,
+	deviceId?: string
 ) => {
-  let device;
-  device = devices.find((dev) => {
-    const customData = dev.customData as HassCustomDeviceData;
-    return (
-      customData &&
-      "webhookId" in customData &&
-      customData.proxyDeviceId === deviceId
-    );
-  });
+	let device;
+	device = devices.find((dev) => {
+		const customData = dev.customData as HassCustomDeviceData;
+		return (
+			customData &&
+			"webhookId" in customData &&
+			customData.proxyDeviceId === deviceId
+		);
+	});
 
-  if (!device) {
-    console.log(requestId, "Unable to find HASS connection info.", devices);
-    throw new IntentFlow.HandlerError(
-      requestId,
-      "invalidRequest",
-      "Unable to find HASS connection info."
-    );
-  }
+	if (!device) {
+		console.log(requestId, "Unable to find HASS connection info.", devices);
+		throw new IntentFlow.HandlerError(
+			requestId,
+			"invalidRequest",
+			"Unable to find HASS connection info."
+		);
+	}
 
-  return device.customData as HassCustomDeviceData;
+	return device.customData as HassCustomDeviceData;
 };
 
 const createResponse = (
-  request: smarthome.IntentRequest,
-  payload: smarthome.IntentResponse["payload"]
+	request: smarthome.IntentRequest,
+	payload: smarthome.IntentResponse["payload"]
 ): any => ({
-  intent: request.inputs[0].intent,
-  requestId: request.requestId,
-  payload,
+	intent: request.inputs[0].intent,
+	requestId: request.requestId,
+	payload,
 });
 
 interface HassCustomDeviceData {
-  webhookId: string;
-  httpPort: number;
-  httpSSL: boolean;
-  baseUrl?: string;
-  uuid?: string;
-  proxyDeviceId: string;
+	webhookId: string;
+	httpPort: number;
+	httpSSL: boolean;
+	baseUrl?: string;
+	uuid?: string;
+	proxyDeviceId: string;
 }
 
 class UnknownInstance extends Error {
-  constructor(public requestId: string) {
-    super();
-  }
+	constructor(public requestId: string) {
+		super();
+	}
 
-  throwHandlerError() {
-    throw new IntentFlow.HandlerError(
-      this.requestId,
-      "invalidRequest",
-      "Unknown Instance"
-    );
-  }
+	throwHandlerError() {
+		throw new IntentFlow.HandlerError(
+			this.requestId,
+			"invalidRequest",
+			"Unknown Instance"
+		);
+	}
 }
 
 const forwardRequest = async (
-  hassDeviceData: HassCustomDeviceData,
-  targetDeviceId: string,
-  request: smarthome.IntentRequest
+	hassDeviceData: HassCustomDeviceData,
+	targetDeviceId: string,
+	request: smarthome.IntentRequest
 ) => {
-  const command = new DataFlow.HttpRequestData();
-  command.method = Constants.HttpOperation.POST;
-  command.requestId = request.requestId;
-  command.deviceId = targetDeviceId;
-  command.isSecure = hassDeviceData.httpSSL;
-  command.port = hassDeviceData.httpPort;
-  command.path = `/api/webhook/${hassDeviceData.webhookId}`;
-  command.data = JSON.stringify(request);
-  command.dataType = "application/json";
+	const command = new DataFlow.HttpRequestData();
+	command.method = Constants.HttpOperation.POST;
+	command.requestId = request.requestId;
+	command.deviceId = targetDeviceId;
+	command.isSecure = hassDeviceData.httpSSL;
+	command.port = hassDeviceData.httpPort;
+	command.path = `/api/webhook/${hassDeviceData.webhookId}`;
+	command.data = JSON.stringify(request);
+	command.dataType = "application/json";
 
-  console.log(request.requestId, "Sending", command);
+	console.log(request.requestId, "Sending", command);
 
-  const deviceManager = await app.getDeviceManager();
+	const deviceManager = await app.getDeviceManager();
 
-  let resp: HttpResponseData;
+	let resp: HttpResponseData;
 
-  try {
-    resp = await new Promise<HttpResponseData>((resolve, reject) => {
-      setTimeout(() => reject(-1), 10000);
-      deviceManager
-        .send(command)
-        .then((response: any) => resolve(response as HttpResponseData), reject);
-    });
-    // resp = (await deviceManager.send(command)) as HttpResponseData;
-    console.log(request.requestId, "Raw Response", resp);
-  } catch (err) {
-    console.error(request.requestId, "Error making request", err);
-    throw new IntentFlow.HandlerError(
-      request.requestId,
-      "invalidRequest",
-      err === -1 ? "Timeout" : err.message
-    );
-  }
+	try {
+		resp = await new Promise<HttpResponseData>((resolve, reject) => {
+			setTimeout(() => reject(-1), 10000);
+			deviceManager
+				.send(command)
+				.then((response: any) => resolve(response as HttpResponseData), reject);
+		});
+		// resp = (await deviceManager.send(command)) as HttpResponseData;
+		console.log(request.requestId, "Raw Response", resp);
+	} catch (err) {
+		console.error(request.requestId, "Error making request", err);
+		throw new IntentFlow.HandlerError(
+			request.requestId,
+			"invalidRequest",
+			err === -1 ? "Timeout" : err.message
+		);
+	}
 
-  // Response if the webhook is not registered.
-  if (resp.httpResponse.statusCode === 200 && !resp.httpResponse.body) {
-    throw new UnknownInstance(request.requestId);
-  }
+	// Response if the webhook is not registered.
+	if (resp.httpResponse.statusCode === 200 && !resp.httpResponse.body) {
+		throw new UnknownInstance(request.requestId);
+	}
 
-  try {
-    const response = JSON.parse(resp.httpResponse.body as string);
+	try {
+		const response = JSON.parse(resp.httpResponse.body as string);
 
-    // Local SDK wants this.
-    response.intent = request.inputs[0].intent;
+		// Local SDK wants this.
+		response.intent = request.inputs[0].intent;
 
-    console.log(request.requestId, "Response", response);
-    return response;
-  } catch (err) {
-    console.error(request.requestId, "Error parsing body", err);
+		console.log(request.requestId, "Response", response);
+		return response;
+	} catch (err) {
+		console.error(request.requestId, "Error parsing body", err);
 
-    throw new IntentFlow.HandlerError(
-      request.requestId,
-      "invalidRequest",
-      err.message
-    );
-  }
+		throw new IntentFlow.HandlerError(
+			request.requestId,
+			"invalidRequest",
+			err.message
+		);
+	}
 };
 
 const identifyHandler = async (
-  request: IntentFlow.IdentifyRequest
+	request: IntentFlow.IdentifyRequest
 ): Promise<IntentFlow.IdentifyResponse> => {
-  console.log("IDENTIFY intent:", request);
+	console.log("IDENTIFY intent:", request);
 
-  const deviceToIdentify = request.inputs[0].payload.device;
+	const deviceToIdentify = request.inputs[0].payload.device;
 
-  if (!deviceToIdentify.mdnsScanData) {
-    console.error(request.requestId, "No usable mdns scan data");
-    return createResponse(request, {} as any);
-  }
+	if (!deviceToIdentify.mdnsScanData) {
+		console.error(request.requestId, "No usable mdns scan data");
+		return createResponse(request, {} as any);
+	}
 
-  if (
-    !deviceToIdentify.mdnsScanData.serviceName.endsWith(
-      "._home-assistant._tcp.local"
-    )
-  ) {
-    console.error(request.requestId, "Not Home Assistant type");
-    return createResponse(request, {} as any);
-  }
+	if (
+		!deviceToIdentify.mdnsScanData.serviceName.endsWith(
+			"._home-assistant._tcp.local"
+		)
+	) {
+		console.error(request.requestId, "Not Home Assistant type");
+		return createResponse(request, {} as any);
+	}
 
-  try {
-    const hassCustomData = findHassCustomDeviceDataByMdnsData(
-      request.requestId,
-      request.devices,
-      deviceToIdentify.mdnsScanData.txt
-    );
-    return await forwardRequest(hassCustomData, "", request);
-  } catch (err) {
-    if (err instanceof UnknownInstance) {
-      return createResponse(request, {} as any);
-    }
-    throw err;
-  }
+	try {
+		const hassCustomData = findHassCustomDeviceDataByMdnsData(
+			request.requestId,
+			request.devices,
+			deviceToIdentify.mdnsScanData.txt
+		);
+		return await forwardRequest(hassCustomData, "", request);
+	} catch (err) {
+		if (err instanceof UnknownInstance) {
+			return createResponse(request, {} as any);
+		}
+		throw err;
+	}
 };
 
 const reachableDevicesHandler = async (
-  request: IntentFlow.ReachableDevicesRequest
+	request: IntentFlow.ReachableDevicesRequest
 ): Promise<IntentFlow.ReachableDevicesResponse> => {
-  console.log("REACHABLE_DEVICES intent:", request);
+	console.log("REACHABLE_DEVICES intent:", request);
 
-  const hassCustomData = findHassCustomDeviceDataByDeviceId(
-    request.requestId,
-    request.devices,
-    request.inputs[0].payload.device.id
-  );
+	const hassCustomData = findHassCustomDeviceDataByDeviceId(
+		request.requestId,
+		request.devices,
+		request.inputs[0].payload.device.id
+	);
 
-  try {
-    return forwardRequest(
-      hassCustomData,
-      // Old code would sent it to the proxy ID: hassCustomData.proxyDeviceId
-      // But tutorial claims otherwise, but maybe it is not for hub devices??
-      // https://developers.google.com/assistant/smarthome/develop/local#implement_the_execute_handler
+	try {
+		return forwardRequest(
+			hassCustomData,
+			// Old code would sent it to the proxy ID: hassCustomData.proxyDeviceId
+			// But tutorial claims otherwise, but maybe it is not for hub devices??
+			// https://developers.google.com/assistant/smarthome/develop/local#implement_the_execute_handler
 
-      // Sending it to the device that has to receive the command as per the tutorial
-      request.inputs[0].payload.device.id as string,
-      request
-    );
-  } catch (err) {
-    if (err instanceof UnknownInstance) {
-      err.throwHandlerError();
-    }
-    throw err;
-  }
+			// Sending it to the device that has to receive the command as per the tutorial
+			request.inputs[0].payload.device.id as string,
+			request
+		);
+	} catch (err) {
+		if (err instanceof UnknownInstance) {
+			err.throwHandlerError();
+		}
+		throw err;
+	}
 };
 
 const executeHandler = async (
-  request: IntentFlow.ExecuteRequest
+	request: IntentFlow.ExecuteRequest
 ): Promise<IntentFlow.ExecuteResponse> => {
-  console.log("EXECUTE intent:", request);
+	console.log("EXECUTE intent:", request);
 
-  const device = request.inputs[0].payload.commands[0].devices[0];
+	const device = request.inputs[0].payload.commands[0].devices[0];
 
-  try {
-    return forwardRequest(
-      device.customData as HassCustomDeviceData,
-      device.id,
-      request
-    );
-  } catch (err) {
-    if (err instanceof UnknownInstance) {
-      err.throwHandlerError();
-    }
-    throw err;
-  }
+	try {
+		return forwardRequest(
+			device.customData as HassCustomDeviceData,
+			device.id,
+			request
+		);
+	} catch (err) {
+		if (err instanceof UnknownInstance) {
+			err.throwHandlerError();
+		}
+		throw err;
+	}
 };
 
 const queryHandler = async (
@@ -276,26 +276,26 @@ const queryHandler = async (
 const app = new App("1.1.0");
 
 app
-  .onIdentify(identifyHandler)
-  .onReachableDevices(reachableDevicesHandler)
-  .onExecute(executeHandler)
+	.onIdentify(identifyHandler)
+	.onReachableDevices(reachableDevicesHandler)
+	.onExecute(executeHandler)
 	.onQuery(queryHandler)
 
-  // @ts-ignore
-  .onIndicate((req) => console.log("Indicate", req))
-  // @ts-ignore
-  .onParseNotification((req) => console.log("ParseNotification", req))
-  // @ts-ignore
-  .onProvision((req) => console.log("Provision", req))
-  // @ts-ignore
-  .onRegister((req) => console.log("Register", req))
-  // @ts-ignore
-  .onUnprovision((req) => console.log("Unprovision", req))
-  // @ts-ignore
-  .onUpdate((req) => console.log("Update", req))
+	// @ts-ignore
+	.onIndicate((req) => console.log("Indicate", req))
+	// @ts-ignore
+	.onParseNotification((req) => console.log("ParseNotification", req))
+	// @ts-ignore
+	.onProvision((req) => console.log("Provision", req))
+	// @ts-ignore
+	.onRegister((req) => console.log("Register", req))
+	// @ts-ignore
+	.onUnprovision((req) => console.log("Unprovision", req))
+	// @ts-ignore
+	.onUpdate((req) => console.log("Update", req))
 
-  .listen()
-  .then(() => {
-    console.log("Ready!");
-  })
-  .catch((e: Error) => console.error(e));
+	.listen()
+	.then(() => {
+		console.log("Ready!");
+	})
+	.catch((e: Error) => console.error(e));
